630fc20c9eeed51d25c31b58b64e732c6fcd125d
ashair12 <ashairimran@gmail.com>
2024-04-03 13:44:20 -0400

    resume section
    completed resume tab

---
+++ scripts.js
@@ -0,0 +1,14 @@
+document.getElementById("homeButton").onclick = function () {
+      location.href = "index.html";
+  };
+
+  document.getElementById("aboutButton").onclick = function () {
+      location.href = "about.html";
+  };
+
+  document.getElementById("contactButton").onclick = function () {
+      location.href = "contact.html";
+  };
+  document.getElementById("featuresButton").onclick = function () {
+    location.href = "features.html";
+};
